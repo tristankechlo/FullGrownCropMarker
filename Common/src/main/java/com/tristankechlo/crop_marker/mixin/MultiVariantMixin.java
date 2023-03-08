536dea9ccff74f91b772d89e83fa2471ee40c599
Tristan Kechlo <66692834+tristankechlo@users.noreply.github.com>
2023-03-08 13:02:21 +0100

    port newer features to 1.19

--- Common/src/main/java/com/tristankechlo/crop_marker/mixin/MultiVariantMixin.java
+++ Common/src/main/java/com/tristankechlo/crop_marker/mixin/MultiVariantMixin.java
@@ -1,6 +1,5 @@
 package com.tristankechlo.crop_marker.mixin;
 
-import com.tristankechlo.crop_marker.types.MarkerPosition;
 import com.tristankechlo.crop_marker.util.ResourceLocationHelper;
 import net.minecraft.client.renderer.block.model.MultiVariant;
 import net.minecraft.client.renderer.block.model.Variant;
@@ -24,14 +23,15 @@ public abstract class MultiVariantMixin {
 
     @Inject(method = "bake", at = @At("HEAD"))
     private void FullGrownCropMarker$bake(ModelBakery $$0, Function<Material, TextureAtlasSprite> $$1, ModelState $$2, ResourceLocation id, CallbackInfoReturnable<BakedModel> cir) {
-        MarkerPosition marker = MarkerPosition.fromId(id);
-        if (marker != MarkerPosition.NONE) {
+        boolean shouldHaveMarker = ResourceLocationHelper.FullGrownCropMarker$shouldHaveMarker(id);
+        if (shouldHaveMarker) {
             this.getVariants().forEach(variant -> {
                 //reapply the marker to all the variants of this model
                 //this is needed because the models are loaded as a WeightedUnbakedModel from the blockstates json
                 //the WeightedUnbakedModel then loads the variant models from the actual models json
                 ResourceLocationHelper location = (ResourceLocationHelper) variant.getModelLocation();
-                location.FullGrownCropMarker$setMarkerPosition(((ResourceLocationHelper) id).FullGrownCropMarker$getMarkerPosition());
+                boolean hasMarker = ((ResourceLocationHelper) id).FullGrownCropMarker$shouldHaveMarker();
+                location.FullGrownCropMarker$setShouldHaveMarker(hasMarker);
             });
         }
     }
